6932323fb537109b55211da0d3f262d36c96970c
taot <1624024615@qq.com>
2025-06-16 18:33:00 +0800

    [feat](cpp_basic): add docs/cpp_basic_structure.md

---
+++ cpp_basic/src/010_goto.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    LOOP: do {
+        if (a == 15) {
+            a += 1;
+            goto LOOP;
+        }
+        cout << "a: " << a << endl;
+        a += 1;
+    } while(a < 20);
+    return 0;
+}
